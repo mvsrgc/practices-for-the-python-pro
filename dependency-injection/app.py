d0b36823cc48608b7026fd956e757cc1feebdb36
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-10-11 15:12:00 -0400

    Decouple data source from app.py.

--- dependency-injection/app.py
+++ dependency-injection/app.py
@@ -9,17 +9,11 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 
 class App:
-    def read(self, file_name):
-        temperatures_by_hour = {}
-        with open(Path(BASE_DIR).joinpath(file_name), "r") as file:
-            reader = csv.reader(file)
-            next(reader)  # Skip header row
-            for row in reader:
-                hour = datetime.datetime.strptime(row[0], "%d/%m/%Y %H:%M").isoformat()
-                temperature = float(row[2])
-                temperatures_by_hour[hour] = temperature
+    def __init__(self, data_source):
+        self.data_source = data_source
 
-        return temperatures_by_hour
+    def read(self, **kwargs):
+        return self.data_source.read(**kwargs)
 
     def draw(self, temperatures_by_hour):
         dates = []
@@ -33,9 +27,12 @@ class App:
         matplotlib.pyplot.plot_date(dates, temperatures, linestyle="-")
         matplotlib.pyplot.show()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import sys
+    from urban_climate_csv import DataSource
+
     file_name = sys.argv[1]
-    app = App()
-    temperatures_by_hour = app.read(file_name)
+    app = App(DataSource())
+    temperatures_by_hour = app.read(file_name=file_name)
     app.draw(temperatures_by_hour)
--- dependency-injection/test_app.py
+++ dependency-injection/test_app.py
@@ -18,11 +18,14 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 
 def test_read():
-    app = App()
+    hour = datetime.datetime.now().isoformat()
+    temperature = 14.52
+    temperature_by_hour = {hour: temperature}
 
-    for key, value in app.read(file_name=Path(BASE_DIR).joinpath("london.csv")).items():
-        assert datetime.datetime.fromisoformat(key)
-        assert value - 0 == value
+    data_source = MagicMock()
+    data_source.read.return_value = temperature_by_hour
+    app = App(data_source=data_source)
+    assert app.read(file_name="something.csv") == temperature_by_hour
 
 
 def test_draw(monkeypatch):
@@ -31,7 +34,7 @@ def test_draw(monkeypatch):
     monkeypatch.setattr(matplotlib.pyplot, "plot_date", plot_date_mock)
     monkeypatch.setattr(matplotlib.pyplot, "show", show_mock)
 
-    app = App()
+    app = App(MagicMock())
     hour = datetime.datetime.now().isoformat()
     temperature = 14.52
     app.draw({hour: temperature})
